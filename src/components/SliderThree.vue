94cfa0c3de5050281e18bbc918e649010013a206
alejandro1osorio <alejowosorio2986@gmail.com>
2025-02-21 15:46:51 -0500

    slider 3

--- src/components/SliderThree.vue
+++ src/components/SliderThree.vue
@@ -6,12 +6,12 @@
     </div>
 
     <!-- Botones de navegación -->
-    <button class="nav-button left" @click="prevPage" :disabled="currentPage === 0">
+    <button class="nav-button left" @click="prevPage">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="hotpink" width="24px" height="24px">
         <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6z"/>
       </svg>
     </button>
-    <button class="nav-button right" @click="nextPage" :disabled="currentPage === pages.length - 1">
+    <button class="nav-button right" @click="nextPage">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="hotpink" width="24px" height="24px">
         <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6z"/>
       </svg>
@@ -40,14 +40,10 @@ export default {
   },
   methods: {
     nextPage() {
-      if (this.currentPage < this.pages.length - 1) {
-        this.currentPage++;
-      }
+      this.currentPage = (this.currentPage + 1) % this.pages.length;
     },
     prevPage() {
-      if (this.currentPage > 0) {
-        this.currentPage--;
-      }
+      this.currentPage = (this.currentPage - 1 + this.pages.length) % this.pages.length;
     },
   },
 };
@@ -69,7 +65,6 @@ export default {
 .slider {
   width: 100%;
   height: auto; /* Ajuste de altura interna */
-  /* border: 1px solid #ccc; */
   display: flex;
   align-items: center;
   justify-content: center;
@@ -103,34 +98,8 @@ export default {
   right: 10px;
 }
 
-.nav-button:disabled {
-  opacity: 0.5;
-  cursor: not-allowed;
-}
-
 /* Responsividad */
-.slider-container {
-  height: auto; /* Ajuste también para la responsividad */
-  background: #804680fa;
-
-}
-
-.slider {
-  height: auto;
-  /* max-height: 700px; */
-}
-
-.nav-button {
-  width: 40px;
-  height: 40px;
-  font-size: 1.2rem;
-}
-
 @media (max-width: 768px) {
-  .slider {
-    height: 90%;
-  }
-
   .nav-button {
     width: 35px;
     height: 35px;
