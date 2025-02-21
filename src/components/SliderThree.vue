22ac16d1ad75f0e4206f1f5fd35225b4504ff7d3
alejandro1osorio <alejowosorio2986@gmail.com>
2025-02-21 16:18:10 -0500

    eliminación

--- src/components/SliderThree.vue
+++ src/components/SliderThree.vue
@@ -23,19 +23,17 @@
 // Importamos los componentes de las páginas
 import PageOne from '../components/slideThree/PageOne.vue';
 import PageTwo from '../components/slideThree/PageTwo.vue';
-import PageThree from '../components/slideThree/PageThree.vue';
 
 export default {
   name: 'Slider',
   components: {
     PageOne,
     PageTwo,
-    PageThree,
   },
   data() {
     return {
       currentPage: 0, // Página inicial
-      pages: ['PageOne', 'PageTwo', 'PageThree'], // Lista de componentes
+      pages: ['PageOne', 'PageTwo'], // Lista de componentes
     };
   },
   methods: {
